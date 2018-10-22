6e618e6766060f5a7d1db87ef8709f882dce314e
Maria Luiza <mariacalixto@cc.ci.ufpb.br>
2018-10-22 20:51:56 -0300

    add movimento da rainha

--- Rainha.java
+++ Rainha.java
@@ -19,5 +19,55 @@ public class Rainha extends Peca
          tabuleiro = new Tabuleiro();
     }
     
-    
+    public Boolean podeMover(Casa destino) {
+        int xOrigem = casa.getX();
+        int yOrigem = casa.getY();
+        int xDestino = destino.getX();
+        int yDestino = destino.getY();
+        
+        if ((tipo == 8 || tipo == 9) && (destino.possuiPeca() == false || capturar(destino))) {
+            // determina yOrigem como menor valor e yDestino com o maior valor
+            // ajuda a percorrer o for do menor para o maior
+            if (xOrigem > xDestino) {
+                int valorMaior = xOrigem;
+                xOrigem = xDestino;
+                xDestino = valorMaior;
+            }
+            if (yOrigem > yDestino) {
+                int valorMaior = yOrigem;
+                yOrigem = yDestino;
+                yDestino = valorMaior;
+            }
+            if (xDestino == xOrigem){
+                // determina yOrigem como menor valor e yDestino com o maior valor
+                // ajuda a percorrer o for do menor para o maior
+                
+                // vasculhar as casas até o yDestino verificando se tem peca adversaria, se tiver, nao pode mover
+                for (int i = yOrigem; i < yDestino; i++) {
+                    if (tabuleiro.getCasa(xOrigem, i).possuiPeca() && tabuleiro.getCasa(xOrigem, i).getTipoPeca()%2 != casa.getTipoPeca()%2) {
+                            return false;
+                    }
+                }
+            }
+            else if (yDestino == yOrigem) {
+                // vasculhar as casas até o xDestino verificando se tem peca adversaria, se tiver, nao pode mover
+                for (int i = xOrigem; i < xDestino; i++) {
+                    if (tabuleiro.getCasa(i, yOrigem).possuiPeca() && tabuleiro.getCasa(i, yOrigem).getTipoPeca()%2 != casa.getTipoPeca()%2) {
+                            return false;
+                    }
+                }
+            }
+            else {                
+                for (int i = xOrigem; i < xDestino; i++) {
+                    for (int j = yOrigem; j < yDestino; j++) {
+                        if ((tabuleiro.getCasa(i, yOrigem).possuiPeca() && tabuleiro.getCasa(i, yOrigem).getTipoPeca()%2 != casa.getTipoPeca()%2)
+                             && (tabuleiro.getCasa(xOrigem, j).possuiPeca() && tabuleiro.getCasa(xOrigem, j).getTipoPeca()%2 != casa.getTipoPeca()%2)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
