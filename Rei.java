7d71f30eddaf0d9eaadd2d8a03a499d589bd4ede
VanessaPessoa <43974084+VanessaPessoa@users.noreply.github.com>
2018-10-25 13:03:11 -0300

    Update Rei.java

--- Rei.java
+++ Rei.java
@@ -6,31 +6,28 @@
  * @version (a version number or a date)
  */
 public class Rei extends Peca
+    private boolean jogada;
+
 {
 
     public Rei(Casa casa, int tipo, Tabuleiro tabuleiro)
     {
        super(casa, tipo, tabuleiro);
+       jogada = false;
     }
       
     public void mover(Casa destino) {
         //movimentação do rei branco e preto
-        if(tipo == 10 || tipo == 11){
-          if (destino.possuiPeca() == false ||  capturar(destino) == true){
-            if((casa.getX()+1 == destino.getX() || casa.getX()-1 == destino.getX() || casa.getX() == destino.getX())
-                                            &&
-            (casa.getY()+1 == destino.getY() || casa.getY()-1 == destino.getY() || casa.getY() == destino.getY())){
-                destino.colocarPeca(this);
-                     casa.removerPeca();
-                     casa=destino;
-            }
-            
-          }
-          
-        }        
+       //movimentação do rei branco e preto
+        if((tipo == 10 || tipo == 11) && podeMover(destino)){
+            casa.removerPeca();
+            destino.colocarPeca(this);
+            casa = destino;
+            jogada = true;
+        }   
      }
     
-    public Boolean podeMover(Casa destino){
+    public boolean podeMover(Casa destino){
         
         if(tipo == 10 || tipo == 11 && (destino.possuiPeca() == false ||  capturar(destino) == true)&&
         (casa.getX()+1 == destino.getX() || casa.getX()-1 == destino.getX() || casa.getX() == destino.getX()) &&
